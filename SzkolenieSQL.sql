ed4de347e73d7dc930375ccc2ea1e9b50cce2fba
Damian Orzeł <damian.orz@wp.pl>
2021-03-22 19:07:54 +0100

    zadania 10-19

--- SzkolenieSQL.sql
+++ SzkolenieSQL.sql
@@ -12,5 +12,21 @@ select distinct department_name from departments;
 select city, nvl(state_province,'Brak') lokalizacja from locations;
 --11
 SELECT distinct department_id from employees order by department_id desc nulls last;
-
-
+--12
+select * from employees where  manager_id = '123';
+--13
+select * from employees where (salary*12)>8000;
+--14
+select * from employees where salary>1100;
+--15
+select * from employees where (12*salary) between 9000 and 13000;
+select * from employees where (12*salary) > 9000 and (12*salary) <13000;
+--16
+select * from employees where salary > 800 and salary < 1200;
+select * from employees order by salary;
+--17
+select * from employees where first_name like '%a';
+--18
+select * from employees where last_name like '%a%' and salary>8000;
+--19
+select * from employees where manager_id in ('100','123');
