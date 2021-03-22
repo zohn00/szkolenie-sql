3bcc19f353c48b062f296b06fcc9248b204666d5
Damian Orzeł <damian.orz@wp.pl>
2021-03-22 20:11:25 +0100

    zadania do 28

--- SzkolenieSQL.sql
+++ SzkolenieSQL.sql
@@ -30,3 +30,22 @@ select * from employees where first_name like '%a';
 select * from employees where last_name like '%a%' and salary>8000;
 --19
 select * from employees where manager_id in ('100','123');
+--20
+select lower(first_name), upper(last_name)from employees;
+--21 -INITCAP
+select initcap(lower(first_name)) from employees;
+--22
+select last_name, replace(last_name,'Ma','AM') from employees;
+--23
+select last_name,substr(last_name, 3,3) from employees;
+--24
+select first_name, to_char(salary, '999999999.99') from employees;
+--25
+select last_name, length(last_name) dlugosc from employees;
+--26
+select first_name, ROUND(salary,2)from employees;
+--27
+select to_char(LAST_DAY(to_date('2098-02-01','YYYY-MM-DD')),'DD') ile_dni_luty from dual;
+--28
+select round(add_months(sysdate,148),'YEAR') from dual;
+
