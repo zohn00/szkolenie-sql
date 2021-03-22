ac1a2b3cf1ae9a770f5655399c8ee6d95d6423a0
Damian Orzeł <damian.orz@wp.pl>
2021-03-22 18:57:32 +0100

    Zadania 1-11

--- SzkolenieSQL.sql
+++ SzkolenieSQL.sql
@@ -1,1 +1,16 @@
 select * from dual;
+
+--3
+desc departments;
+
+--8
+select * from departments;
+--9
+select distinct department_name from departments;
+
+--10
+select city, nvl(state_province,'Brak') lokalizacja from locations;
+--11
+SELECT distinct department_id from employees order by department_id desc nulls last;
+
+
